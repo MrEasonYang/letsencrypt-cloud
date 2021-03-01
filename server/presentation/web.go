7915595baafa02180cd9b34985030e08a154314f
Eason Yang <me@easonyang.com>
2021-03-01 23:39:49 +0800

    Init apis.

---
+++ server/presentation/web.go
@@ -0,0 +1,31 @@
+package presentation
+
+import "github.com/gin-gonic/gin"
+
+func Login(context *gin.Context) {
+
+}
+
+func Logout(context *gin.Context) {
+
+}
+
+func ListSlavesByPage(context *gin.Context) {
+
+}
+
+func CountAllSlaves(context *gin.Context) {
+
+}
+
+func CreateSlave(context *gin.Context) {
+
+}
+
+func RemoveSlave(context *gin.Context) {
+
+}
+
+func UpdateSlave(context *gin.Context) {
+
+}
