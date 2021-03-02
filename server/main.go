a19577b1a2540ddd1ff93e647682162fb9331d4b
Eason Yang <me@easonyang.com>
2021-03-02 23:33:51 +0800

    Update api, init client.

--- server/main.go
+++ server/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/MrEasonYang/letsencrypt-cloud/server/presentation"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/login", presentation.Login)
+	r.POST("/logout", presentation.Logout)
+	r.GET("/list-slaves", presentation.ListSlavesByPage)
+	r.GET("/count-slaves", presentation.CountAllSlaves)
+	r.POST("/create-slave", presentation.CreateSlave)
+	r.POST("/remove-slave", presentation.RemoveSlave)
+	r.POST("/update-slave", presentation.UpdateSlave)
+	r.POST("/mannual-sync", presentation.MannualSync)
 	r.Run()
 }
--- server/presentation/web.go
+++ server/presentation/web.go
@@ -2,30 +2,42 @@ package presentation
 
 import "github.com/gin-gonic/gin"
 
+// Login is the func which handle user login.
 func Login(context *gin.Context) {
 
 }
 
+// Logout is the func which handle user logout.
 func Logout(context *gin.Context) {
 
 }
 
+// ListSlavesByPage is the func which can list and paginate all slaves.
 func ListSlavesByPage(context *gin.Context) {
 
 }
 
+// CountAllSlaves is the func cooperate with ListSlavesByPages.
 func CountAllSlaves(context *gin.Context) {
 
 }
 
+// CreateSlave is the func which do the slave creation.
 func CreateSlave(context *gin.Context) {
 
 }
 
+// RemoveSlave is the func which do the slave removal.
 func RemoveSlave(context *gin.Context) {
 
 }
 
+// UpdateSlave is the func which do the slave update.
 func UpdateSlave(context *gin.Context) {
+
+}
+
+// MannualSync is the func which sync data to slaves.
+func MannualSync(context *gin.Context) {
 
 }
