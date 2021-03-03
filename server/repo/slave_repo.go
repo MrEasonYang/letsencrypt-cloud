39c4187ac55d5f513fbbe9a1ab15efeddf995239
Eason Yang <me@easonyang.com>
2021-03-04 02:04:20 +0800

    Add slave repo.

--- server/repo/slave_repo.go
+++ server/repo/slave_repo.go
@@ -1,1 +1,21 @@
 package repo
+
+// GetSlave is the func to get the specified slave.
+func GetSlave() {
+
+}
+
+// GetSlaves is the func to get a group of slaves.
+func GetSlaves() {
+
+}
+
+// DeleteSlave is the func to delete a slave.
+func DeleteSlave() {
+
+}
+
+// SaveSlave is the func to add or update a slave.
+func SaveSlave() {
+
+}
