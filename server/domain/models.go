f66c3418f184afee9fdeac916d56761e51df8217
Eason Yang <me@easonyang.com>
2021-03-05 23:50:27 +0800

    Add modles

--- server/domain/models.go
+++ server/domain/models.go
@@ -1,1 +1,20 @@
 package domain
+
+import "golang.org/x/text/date"
+
+type User struct {
+	ID int
+	Username string
+	Password string
+	Salt string
+	Email string
+}
+
+type Slave struct {
+	ID int
+	IP string
+	EncryptKey string
+	Version int
+	LastSyncTime string
+}
+
